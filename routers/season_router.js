a525663652fd855b36cac1f484f62ac0d0dd0e91
pparking <pparking@pparkings-MacBook-Air.local>
2020-05-26 02:15:43 +0800

    renew batch

--- routers/season_router.js
+++ routers/season_router.js
@@ -103,11 +103,11 @@ season_router.route('/list_season_for_admin')
 })
 
 
-season_router.route('/renew_season_batch')
+season_router.route('/renew_season_batch_admin')
 .post(verify_admin, async (req, res, next) => {
     try {
         req.body.data.user = req.user
-        req.data = await season.renew_season_batch(req.body.data)
+        req.data = await season.renew_season_batch_admin(req.body.data)
         next()
     } catch (err) {
         throw err
