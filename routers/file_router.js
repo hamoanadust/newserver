0c1333968b3f19ab3757f5ce248028e7686ad45f
pparking <pparking@pparkings-MacBook-Air.local>
2020-05-31 15:23:12 +0800

    upload

--- routers/file_router.js
+++ routers/file_router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const file_router = express.Router()
-// const file = require('../services/file')
+const file = require('../services/file')
 const { verify_user, verify_admin } = require('../services/auth')
 
 file_router.use(fileUpload({
@@ -12,18 +12,7 @@ file_router.use(fileUpload({
 file_router.route('/upload')
 .post(verify_user, async (req, res, next) => {
     try {
-        console.log(req.body)
-        if(!req.files) throw new Error('No file uploaded')
-        let file = req.files.file
-        console.log(file.name)
-        const arr = file.name.split('_')
-        console.log(arr)
-        file.mv(`./uploads/${arr[0]}/${req.user.user_id}/${arr[1]}`)
-        req.data = {
-            name: file.name,
-            mimetype: file.mimetype,
-            size: file.size
-        }
+        req.data = await file.upload(req)
         next()
     } catch (err) {
         req.data = err
