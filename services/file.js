5c4a7cf95003ae8c3dab3a31ec3431e87b543cf0
pparking <pparking@pparkings-MacBook-Air.local>
2020-05-31 16:01:20 +0800

    upload

--- services/file.js
+++ services/file.js
@@ -11,7 +11,7 @@ const upload = data => {
         const exist = fs.existsSync(path.join(__dirname, `/../uploads/${carpark_id}/${user_id}`))
         if (!exist) {
             console.log(path.join(__dirname, `/../uploads/${carpark_id}/${user_id}`))
-            fs.mkdirSync(path.join(__dirname, `/../uploads/${carpark_id}/${user_id}`))
+            fs.mkdirSync(path.join(__dirname, `/../uploads/${carpark_id}/${user_id}`), { recursive: true })
             console.log('mkdir', `./uploads/${carpark_id}/${user_id}`)
         }
         file.mv(`./uploads/${carpark_id}/${user_id}/${file.name}`)
